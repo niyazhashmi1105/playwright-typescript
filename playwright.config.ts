4aa636e8f30d608512cd9454c19a18d1b86b9f22
MD Niyaz Hashmi <mdniyazhashmi@example.com>
2025-05-03 09:36:41 +0530

    updating jenkins prometheus endpoint

--- playwright.config.ts
+++ playwright.config.ts
@@ -45,7 +45,7 @@ export default defineConfig({
   timeout: 60 * 1000,
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
-  retries: process.env.CI ? 0 : 0,
+  retries: process.env.CI ? 1 : 0,
   workers: process.env.CI ? 3 : 3,
   reporter: [
     ['html'],
